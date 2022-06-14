cf318d1639ba5154dc45a95af104666fcd78cc86
Ryabaa <you@example.com>
2022-06-14 21:33:32 +0300

    add redesign

--- src/components/layout/Game.jsx
+++ src/components/layout/Game.jsx
@@ -2,7 +2,9 @@ import { useContext } from "react";
 import { GameContext } from "../game/GameContext.jsx";
 
 import { IconContext } from "react-icons/lib";
-import { IoReload } from "react-icons/io5";
+import { RiArrowGoBackLine } from "react-icons/ri";
+import { CgMenu } from "react-icons/cg";
+import { VscDebugRestart } from "react-icons/vsc";
 
 import Piece from "./Piece.jsx";
 
@@ -16,11 +18,11 @@ function Game() {
                 <div className="score">
                     <div className="score-container">
                         <p className="score__text">Score</p>
-                        <p className="score__number">21332</p>
+                        <p className="score__number">666</p>
                     </div>
                     <div className="score-container">
                         <p className="score__text">Best</p>
-                        <p className="score__number">0</p>
+                        <p className="score__number">777</p>
                     </div>
                 </div>
             </div>
@@ -28,12 +30,24 @@ function Game() {
                 {field.map((row, y) => row.map((number, x) => <Piece key={`${y}-${x}`} number={number} />))}
             </div>
             <div className="nav-container">
-                <button className="nav__res-btn">
-                    <IconContext.Provider value={{ className: "nav__res-btn-icon" }}>
-                        <IoReload />
+                <button className="nav__button">
+                    <IconContext.Provider value={{ className: "nav__button-icon" }}>
+                        <RiArrowGoBackLine />
                     </IconContext.Provider>
+                    <p>Back</p>
                 </button>
-                <button className="nav__menu-btn">Menu</button>
+                <button className="nav__button">
+                    <IconContext.Provider value={{ className: "nav__button-icon" }}>
+                        <CgMenu />
+                    </IconContext.Provider>
+                    <p>Menu</p>
+                </button>
+                <button className="nav__button">
+                    <IconContext.Provider value={{ className: "nav__button-icon" }}>
+                        <VscDebugRestart />
+                    </IconContext.Provider>
+                    <p>Restart</p>
+                </button>
             </div>
         </main>
     );
